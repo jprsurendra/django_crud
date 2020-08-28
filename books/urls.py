ba73d099cbc4574aca02902f5678f4acc644aad5
jprsurendra <jpr_surendra@yahoo.co.in>
2020-08-28 06:40:08 +0530

    add books app

--- books/urls.py
+++ books/urls.py
@@ -4,10 +4,11 @@ from books import views
 urlpatterns = [
     url(r'^$', views.BookList.as_view(), name='book_list'),
     url(r'^view/(?P<pk>\d+)/$', views.BookView.as_view(), name='book_view'),
-    url(r'^new$', views.BookCreate.as_view(), name='book_new'),
+    url(r'^new/$', views.BookCreate.as_view(), name='book_new'),
     url(r'^edit/(?P<pk>\d+)/$', views.BookUpdate.as_view(), name='book_edit'),
     url(r'^delete/(?P<pk>\d+)/$', views.BookDelete.as_view(), name='book_delete'),
 ]
+
 '''
 url(r'^upload-documents/(?P<pk>\d{0,50})/$', UploadDocumentView.as_view(), name='upload docuemnt'),
 
